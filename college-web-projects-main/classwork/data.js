9c10ad996dbbf5f139ff58e1c5d0d1a2e31e82bd
sujal shrestha <shresthasujal917@gmail.com>
2024-02-21 08:09:29 +0545

    classwork

---
+++ college-web-projects-main/classwork/data.js
@@ -0,0 +1,23 @@
+const users = [
+    {
+    name: "Jayski Oje",
+    office_address: "24, Wall street",
+    home_address: "365 Bridges road",
+    email: "user@example.com",
+    avatar: "https://media.istockphoto.com/id/509275531/photo/car-in-the-rear-view-mirror.webp?b=1&s=170667a&w=0&k=20&c=-NP9MIg8LtoFYJx3HCpE1JpJFXGG96Jhrai5_8EGTwY="
+    },
+    {
+    name: "Bing BoyTheo",
+    office_address: "675, Ratatouille street",
+    home_address: "54 Gates Estate",
+    email: "user@example.com",
+    avatar: "https://cdn.pixabay.com/photo/2015/09/01/03/42/driving-916405_640.jpg"
+    },
+    {
+    name: "Ghost Sam",
+    office_address: "001, Silent street",
+    home_address: "87 North-void Street",
+    email: "user@example.com",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+    },
+    ];
